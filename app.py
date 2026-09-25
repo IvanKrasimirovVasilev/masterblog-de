2f346b07f553862d73a3d0c4483a5e0ff44147a7
Ivan Vasilev <i_vasilev@ymail.com>
2026-09-25 11:54:41 +0200

    create update template and route

--- app.py
+++ app.py
@@ -7,6 +7,16 @@ def load_posts():
         blog_posts = json.load(file)
 
     return blog_posts
+
+def fetch_post_by_id(post_id):
+    blog_posts = load_posts()
+
+    for post in blog_posts:
+        if post['id'] == post_id:
+            return post
+
+    return None
+
 @app.route('/')
 def index():
     blog_posts = load_posts()
@@ -41,6 +51,44 @@ def add_post():
         return redirect(url_for('index'))
 
     return render_template('add-post.html')
+@app.route('/update/<int:post_id>', methods=['GET', 'POST'])
+def update_post(post_id):
+    blog_posts = load_posts()
+
+    post = None
+
+    for blog_post in blog_posts:
+        if blog_post['id'] == post_id:
+            post = blog_post
+            break
+
+    if post is None:
+        return "Post not found", 404
+
+    if request.method == 'POST':
+        post['author'] = request.form.get('author')
+        post['title'] = request.form.get('title')
+        post['content'] = request.form.get('content')
+
+        with open("blog_posts.json", "w") as file:
+            json.dump(blog_posts, file, indent=4)
+
+        return redirect(url_for('index'))
+
+    return render_template('update-post.html', post=post)
+@app.route('/delete/<int:post_id>')
+def delete_post(post_id):
+    blog_posts = load_posts()
+
+    for post in blog_posts:
+        if post['id'] == post_id:
+            blog_posts.remove(post)
+            break
+
+    with open("blog_posts.json", "w") as file:
+        json.dump(blog_posts, file)
+
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
