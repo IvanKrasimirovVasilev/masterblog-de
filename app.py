afa83b9200099922671362ab4509bbe622f3af63
Ivan Vasilev <i_vasilev@ymail.com>
2026-09-25 10:40:08 +0200

    Create styles, templates and blog posts

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
-
+from flask import Flask, render_template
+import json
 app = Flask(__name__)
 
+def load_posts():
+    with open("blog_posts.json", "r") as file:
+        blog_posts = json.load(file)
 
+    return blog_posts
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    blog_posts = load_posts()
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
